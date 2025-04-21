4f5246d60c973710d7494f60121c7e97f9b2ecb0
jhayber17 <jhayberm@unicauca.edu.co>
2025-04-20 22:15:56 -0500

    Feature: eliminar clases no necesarias

--- src/main/java/co/edu/unicauca/autenticacion/usuarios/security/JwtUtil.java
+++ src/main/java/co/edu/unicauca/autenticacion/usuarios/security/JwtUtil.java
@@ -3,8 +3,6 @@ package co.edu.unicauca.autenticacion.usuarios.security;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.security.Key;
@@ -12,8 +10,6 @@ import java.util.*;
 
 @Component
 public class JwtUtil {
-
-    private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
 
     @Value("${app.jwtSecret}")
     private String jwtSecret;
